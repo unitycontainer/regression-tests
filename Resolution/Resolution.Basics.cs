f50823bfcd48d0a69175197a632c4623a95fafd7
Eugene Sadovoi <evgeni@eniks.com>
2020-08-27 16:37:31 -0700

    Adding more tests

---
+++ Resolution/Resolution.Basics.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unity;
+using Unity.Lifetime;
+
+namespace Container.Resolution
+{
+    [TestClass]
+    public class Basics
+    {
+        protected IUnityContainer Container;
+
+        [TestInitialize]
+        public void InitializeTest() => Container = new UnityContainer();
+
+
+        [TestMethod]
+        public void ObjectFromEmptyContainer()
+        {
+            // Act/Verify
+            Assert.IsNotNull(Container.Resolve<object>());
+        }
+
+        [TestMethod]
+        public void ObjectRegistered()
+        {
+            Container.RegisterType<object>(new ContainerControlledLifetimeManager());
+
+            // Act/Verify
+            Assert.IsNotNull(Container.Resolve<object>());
+        }
+
+    }
+}
