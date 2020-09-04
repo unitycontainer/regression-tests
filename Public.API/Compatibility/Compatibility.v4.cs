50ba42425ff54cca9d1c4d4b3785c1ad1e3190fc
Eugene Sadovoi <evgeni@eniks.com>
2020-09-04 16:14:05 -0700

    Refactoring

--- Public.API/Compatibility/Compatibility.v4.cs
+++ Public.API/Compatibility/Compatibility.v4.cs
@@ -114,4 +114,45 @@ namespace Microsoft.Practices.Unity
         
         #endregion
     }
+
+    public static partial class Inject
+    {
+        #region Array
+
+        public static ParameterBase Array(Type elementType, params object[] elementValues)
+            => new ResolvedArrayParameter(elementType, elementValues);
+
+        public static ParameterBase Array<TElement>(params object[] elementValues)
+            => new ResolvedArrayParameter(typeof(TElement), elementValues);
+
+        #endregion
+
+
+        #region Parameter
+
+        public static ParameterBase Parameter(object value) => new InjectionParameter(value);
+
+        public static ParameterBase Parameter(Type type, object value)
+            => new InjectionParameter(type ?? throw new ArgumentNullException(nameof(type)), value);
+        public static ParameterBase Parameter<TTarget>(object value) => new InjectionParameter(typeof(TTarget), value);
+
+        #endregion
+
+
+        #region Field
+
+        public static InjectionMember Field(string name, object value)
+            => new InjectionField(name ?? throw new ArgumentNullException(nameof(name)), value);
+
+        #endregion
+
+
+        #region Property
+
+        public static InjectionMember Property(string name, object value)
+            => new InjectionProperty(name ?? throw new ArgumentNullException(nameof(name)), value);
+
+        #endregion
+    }
+
 }
