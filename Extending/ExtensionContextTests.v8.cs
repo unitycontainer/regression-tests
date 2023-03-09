78665da5ace0ddb66578cd690402e24201e603a4
ENikS <evgeni@eniks.com>
2023-03-09 10:24:52 -0800

    Moving files

---
+++ Extending/ExtensionContextTests.v8.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unity.Regression.Tests;
+#if NET45
+using Microsoft.Practices.ObjectBuilder2;
+using Microsoft.Practices.Unity;
+#else
+using Unity;
+#endif
+
+
+namespace Container.Extending
+{
+    [TestClass]
+    public partial class ExtensionContextTests
+    {
+        ExtensionContext context;   
+        UnityContainer   container;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            container = new UnityContainer();
+            var mock = new MockContainerExtension();
+            container.AddExtension(mock);
+            context = ((IMockConfiguration)mock).Context;
+        }
+
+        [TestMethod]
+        public void BaselineTest() => Assert.IsNotNull(context);
+
+        [TestMethod]
+        public void ContainerTest()
+        {
+            // Validate
+            Assert.IsNotNull(context.Container);
+            Assert.IsInstanceOfType(context.Container, typeof(UnityContainer));
+        }
+
+        [TestMethod]
+        public void PoliciesTest()
+        {
+            // Validate
+            Assert.IsNotNull(context.Policies);
+            Assert.IsInstanceOfType(context.Policies, typeof(IPolicyList));
+        }
+
+    }
+}
